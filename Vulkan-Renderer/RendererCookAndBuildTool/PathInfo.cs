788678b88fb1adf47556bbdbce937b5bfcf331d0
Joseph Kalathil <josephbk.117@hotmail.com>
2021-05-12 03:36:04 +0530

    Initial commit for automating build steps

---
+++ Vulkan-Renderer/RendererCookAndBuildTool/PathInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RendererCookAndBuildTool
+{
+    static class PathInfo
+    {
+        public static readonly string SolutionDirectory = "..\\..\\..";
+        public static readonly string VulkanRendererProjectPath = SolutionDirectory + "\\Vulkan-Renderer";
+        public static readonly string ResourceFolder = VulkanRendererProjectPath + "\\Res";
+        public static readonly string CompiledShadersFolder = ResourceFolder + "\\CompiledShaders";
+        public static readonly string ShadersFolder = ResourceFolder + "\\Shaders";
+        public static readonly string TexturesFolder = ResourceFolder + "\\Textures";
+
+        public static readonly string CompileShadersToSprirVBat;
+
+        public static readonly string BinOutputPathDebug = SolutionDirectory + "\\x64\\Debug\\";
+        public static readonly string BinOutputPathRelease = SolutionDirectory + "\\x64\\Release\\";
+        public static readonly string ResFolderOutputPathDebug = BinOutputPathDebug + "\\Res";
+        public static readonly string ResFolderOutputPathRelease = BinOutputPathRelease + "\\Res";
+
+        public static readonly string DllIGlfwSrcPath = SolutionDirectory + "\\Libs\\glfw3.dll";
+        public static readonly string DllIGlfwDstPathDebug = BinOutputPathDebug + "\\glfw3.dll";
+        public static readonly string DllIGlfwDstPathRelease = BinOutputPathRelease + "\\glfw3.dll";
+    }
+}
---
+++ Vulkan-Renderer/RendererCookAndBuildTool/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RendererCookAndBuildTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(Path.GetFullPath(PathInfo.BinOutputPathDebug));
+            File.Copy(Path.GetFullPath(PathInfo.DllIGlfwSrcPath), Path.GetFullPath(PathInfo.DllIGlfwDstPathDebug), true);
+
+            string resFolderOutputPath = Path.GetFullPath(PathInfo.ResFolderOutputPathDebug);
+
+            List<string> foldersToAddToOutputRes = new List<string>();
+            foldersToAddToOutputRes.Add(PathInfo.CompiledShadersFolder);
+            foldersToAddToOutputRes.Add(PathInfo.TexturesFolder);
+
+            foreach (string inputFolder in foldersToAddToOutputRes)
+            {
+                if (Directory.Exists(inputFolder))
+                {
+                    string[] files = Directory.GetFiles(Path.GetFullPath(inputFolder));
+
+                    foreach (string file in files)
+                    {
+                        string fileName = Path.GetFileName(file);
+                        string outputFolderName = Path.GetFileName(Path.GetDirectoryName(file));
+                        string outputFolderPath = resFolderOutputPath + "\\" + outputFolderName;
+                        string destFile = Path.Combine(outputFolderPath, fileName);
+                        Directory.CreateDirectory(outputFolderPath);
+                        File.Copy(file, destFile, true);
+                    }
+                }
+            }
+
+        }
+    }
+}
