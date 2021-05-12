e8c92499418ffd9d21ba0afdf9f11c09ca15c86a
Joseph Kalathil <josephbk.117@hotmail.com>
2021-05-13 02:17:50 +0530

    Missed changes for prev commit

--- Vulkan-Renderer/RendererCookAndBuildTool/Program.cs
+++ Vulkan-Renderer/RendererCookAndBuildTool/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RendererCookAndBuildTool
 {
@@ -11,6 +9,28 @@ namespace RendererCookAndBuildTool
     {
         static void Main(string[] args)
         {
+            string[] shaderFiles = Directory.GetFiles(Path.GetFullPath(PathInfo.ShadersFolder));
+
+            bool atleastOneShaderFailed = false;
+            Console.WriteLine("GLSLC SpirV compiler process started");
+
+            foreach (string shaderFile in shaderFiles)
+            {
+                string inShaderFile = "\"" + shaderFile + "\"";
+                string outShaderFile = "\"" + Path.GetFullPath(PathInfo.CompiledShadersFolder) + "\\" + Path.GetFileName(shaderFile) + ".spv\"";
+                if (!ExecuteCommand(Path.GetFullPath(PathInfo.CompileShadersToSprirVGlslc), inShaderFile + " -o " + outShaderFile))
+                {
+                    atleastOneShaderFailed = true;
+                }
+            }
+
+            // Skip copying to build folder
+            if(atleastOneShaderFailed)
+            {
+                Console.ReadLine();
+                return;
+            }
+
             Directory.CreateDirectory(Path.GetFullPath(PathInfo.BinOutputPathDebug));
             File.Copy(Path.GetFullPath(PathInfo.DllIGlfwSrcPath), Path.GetFullPath(PathInfo.DllIGlfwDstPathDebug), true);
 
@@ -37,7 +57,48 @@ namespace RendererCookAndBuildTool
                     }
                 }
             }
+        }
 
+        static public bool ExecuteCommand(string exeDir, string args)
+        {
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo
+                {
+                    FileName = exeDir,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = procStartInfo;
+                    process.Start();
+                    process.WaitForExit();
+
+                    Console.WriteLine("Info Output : " + process.StandardOutput.ReadToEnd());
+
+                    string errorStr = process.StandardError.ReadToEnd();
+                    Console.WriteLine("Error Output : " + errorStr);
+
+                    if (errorStr.Length > 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred executing the following commands");
+                Console.WriteLine(exeDir);
+                Console.WriteLine(args);
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
     }
 }
